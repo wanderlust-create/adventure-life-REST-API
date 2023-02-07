885c66ea57221cf675f1efb58798306591d9869d
Tamara Dowis <tamara.dowis@gmail.com>
2023-02-07 14:14:46 -0600

    create server with db access

---
+++ src/api/routes/api/index.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import city from "./city";
+const app = Router();
+city(app);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express from "express";
+
+// Load HTTP request logger
+import morgan from "morgan";
+
+// access .env variables
+import config from "./config";
+
+// load winston logger
+import logger from "./loaders/logger";
+
+// Routes
+import api from "./api";
+
+// Set up database access
+import setupDb from "./loaders/dbSetup";
+setupDb();
+
+
+async function startServer() {
+  const app = express();
+  app.use(express.json());
+  app.use(morgan("combined"));
+  app.use("/", api);
+
+  app.listen(config.PORT, () => {
+    logger.info(`
+      👍 🎁 🎉  Charter Spectrum REST API listening at http://localhost:${config.PORT} 🎉 🎁  👍 
+   
+    `);
+  });
+}
+
+startServer();
