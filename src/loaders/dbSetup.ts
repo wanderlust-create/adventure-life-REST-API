5ba98c7beab0f7a8d16febc42a1dd65fdab70349
Tamara Dowis <tamara.dowis@gmail.com>
2023-03-30 17:43:13 -0600

    add error handling for DB connection

--- src/loaders/dbSetup.ts
+++ src/loaders/dbSetup.ts
@@ -1,9 +1,19 @@
 import knex from "knex";
 import { Model } from "objection";
+import logger from "./logger"
 
 import knexConfig from "../config/knexfile";
 
-export default function setupDb() {
+
+async function setupDb() {
   const db = knex(knexConfig.development);
-  Model.knex(db);
+  try {
+    Model.knex(db);
+    logger.info("DB connected");
+  } catch (error) {
+    logger.error("Could not connect to db");
+    process.exit(1);
+  }
 }
+
+export default setupDb;
