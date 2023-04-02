c3c319cadc228aaa8239f5e880bd78fd7281ef26
Tamara Dowis <tamara.dowis@gmail.com>
2023-04-01 20:40:11 -0600

    update app access for testing

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,20 @@
 import createServer from "./loaders/server";
-
-// access to .env files
 import config from "./config";
-
-// Swagger docs
 import swaggerDocs from "./loaders/swagger";
-
-// Set up database access
 import db from "./loaders/dbSetup";
-
-// load winston logger
 import logger from "./loaders/logger";
 
-// find port
 const port = config.PORT;
 
 const app = createServer();
 
 app.listen(port, async () => {
   swaggerDocs(app, port);
-  logger.info(`🎆 🚕 ✈️  Adventure Life REST API listening at http://localhost:${config.PORT} ✈️ 🚕 🎆
-    `);
+  logger.info(
+    `🎆 🚕 ✈️  Adventure Life REST API listening at http://localhost:${config.PORT} ✈️ 🚕 🎆`
+  );
   db();
   logger.info("Database is connected");
 });
+
+export default app;
