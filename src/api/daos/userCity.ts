0e100c2164b5fa4aa8f0e9fbae7297b5e721d6a6
Tamara Dowis <tamara.dowis@gmail.com>
2023-04-02 19:43:30 -0600

    refactored UserCity dao

--- src/api/daos/userCity.ts
+++ src/api/daos/userCity.ts
@@ -7,12 +7,22 @@ export default {
   deleteUserCityById,
 };
 
-async function listAllUserCities() {
+/**
+ * Returns an array of all userCities, sorted by date.
+ * @returns {Promise<UserCity[]>} The cities.
+ */
+async function listAllUserCities(): Promise<UserCity[]> {
   logger.debug(`Entering GET ALL DAO- user-cities endpoint`);
   return UserCity.query()
     .column("id", "userId", "cityId")
     .orderBy("created_at", "desc");
 }
+
+/**
+ * Creates a new userCity.
+ * @param {userCityDto} userCityDto - The user city data.
+ * @returns {Promise<UserCity>} The new user city.
+ */
 async function createUserCity(userCityDto: UserCity) {
   logger.debug(`Entering CREATE DAO- user-cities endpoint`);
   const newUserCity = await UserCity.query().insert({
@@ -21,6 +31,12 @@ async function createUserCity(userCityDto: UserCity) {
   });
   return newUserCity;
 }
+
+/**
+ * Deletes a userCity by ID.
+ * @param {string} id - The ID of the userCity to delete.
+ * @returns {Promise<UserCity>} The deleted userCity.
+ */
 async function deleteUserCityById(id: string) {
   logger.debug(`Entering DELETE DAO- user-cities endpoint`);
   const deletedUserCity = await UserCity.query()
