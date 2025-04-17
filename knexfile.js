04b9826f5133ad659c4e3697fc6622c05c86d73a
Tamara Dowis <tamara.dowis@gmail.com>
2025-04-16 19:32:40 -0500

    fix: migrate knex configuration to TypeScript for improved compatibility

--- knexfile.js
+++
@@ -1,7 +0,0 @@
-// This file is used to configure Knex.js for database migrations and seeding.
-require('ts-node/register');
-require('dotenv').config(); // 👈 Load .env from project root
-
-const config = require('./src/config/knexConfig.ts').default;
-
-module.exports = config;
