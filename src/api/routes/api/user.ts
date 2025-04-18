eb32a65e1bd8fe46dff82f0bbbdb62cdcbb0e459
Tamara Dowis <tamara.dowis@gmail.com>
2025-04-18 18:14:54 -0500

    refactor: improve Swagger docs

--- src/api/routes/api/user.ts
+++ src/api/routes/api/user.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response, Router } from 'express';
+import { Router } from 'express';
 import UserController from '../../controllers/user';
 
 // Error Handler
@@ -16,14 +16,13 @@ export default (app: Router) => {
    *     User:
    *       type: object
    *       required:
-   *         - id
    *         - firstName
    *         - lastName
    *         - email
    *       properties:
    *         id:
    *           type: number
-   *           description: The auto-generated id of the user
+   *           description: Auto-generated user ID
    *         firstName:
    *           type: string
    *           description: The user's first name
@@ -41,26 +40,7 @@ export default (app: Router) => {
    *     UserArray:
    *       type: array
    *       items:
-   *         type: object
-   *         properties:
-   *         id:
-   *           type: number
-   *           description: The auto-generated id of the user
-   *         firstName:
-   *           type: string
-   *           description: The user's first name
-   *         lastName:
-   *           type: string
-   *           description: The user's last name
-   *         email:
-   *           type: string
-   *           description: The user's email address
-   *         example:
-   *           id: 4
-   *           firstName: Stansie
-   *           lastName: Narayan
-   *           email: Stansie@email.com
-   *
+   *         $ref: '#/components/schemas/User'
    */
 
   /**
@@ -74,20 +54,19 @@ export default (app: Router) => {
    * @swagger
    * /api/v1/users:
    *   get:
-   *     summary: Returns an array of all the users
+   *     summary: Get all users
    *     tags: [Users]
    *     responses:
    *       200:
-   *         description: The users were successfully retrieved
+   *         description: List of users
    *         content:
    *           application/json:
    *             schema:
-   *               type: array
    *               $ref: '#/components/schemas/UserArray'
    *       404:
-   *         description: Users were  not found
+   *         description: No users found
    *       500:
-   *         description: An error occurred
+   *         description: Server error
    */
   route.get('/', UserController.listAllUsers);
 
@@ -95,26 +74,26 @@ export default (app: Router) => {
    * @swagger
    * /api/v1/users/{id}:
    *   get:
-   *     summary: Get user attributes with user_id
+   *     summary: Get user by ID
    *     tags: [Users]
    *     parameters:
    *       - in: path
    *         name: id
+   *         required: true
    *         schema:
    *           type: number
-   *         required: true
-   *         description: The user id
+   *         description: The user ID
    *     responses:
    *       200:
-   *         description: The user attributes
+   *         description: User retrieved
    *         content:
    *           application/json:
    *             schema:
    *               $ref: '#/components/schemas/User'
    *       404:
-   *         description: The user was not found
+   *         description: User not found
    *       500:
-   *         description: An error occurred
+   *         description: Server error
    */
   route.get('/:id', UserController.getUserById);
 
@@ -136,51 +115,51 @@ export default (app: Router) => {
    *             lastName: Seto
    *             email: Sinead@email.com
    *     responses:
-   *       200:
-   *         description: The user was successfully created
+   *       201:
+   *         description: User created successfully
    *         content:
    *           application/json:
    *             schema:
    *               $ref: '#/components/schemas/User'
-   *       404:
-   *         description: The user was not found
+   *       400:
+   *         description: Validation error
    *       500:
-   *         description: An error occurred
+   *         description: Server error
    */
   route.post('/', validateDto(userDto), UserController.createUser);
 
   /**
    * @swagger
    * /api/v1/users/{id}:
-   *  patch:
-   *    summary: Update user using user_id
-   *    tags: [Users]
-   *    parameters:
-   *      - in: path
-   *        name: id
-   *        schema:
-   *          type: number
-   *        required: true
-   *        description: The user id
-   *    requestBody:
-   *      required: true
-   *      content:
-   *        application/json:
-   *          schema:
-   *            $ref: '#/components/schemas/User'
-   *          example:
+   *   patch:
+   *     summary: Update a user by ID
+   *     tags: [Users]
+   *     parameters:
+   *       - in: path
+   *         name: id
+   *         required: true
+   *         schema:
+   *           type: number
+   *         description: The user ID
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/User'
+   *           example:
    *             lastName: Corceda
-   *    responses:
-   *      200:
-   *        description: The user was updated
-   *        content:
-   *          application/json:
-   *            schema:
-   *              $ref: '#/components/schemas/User'
-   *      404:
-   *        description: The user was not found
-   *      500:
-   *        description: An error occurred
+   *     responses:
+   *       200:
+   *         description: User updated successfully
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/User'
+   *       404:
+   *         description: User not found
+   *       500:
+   *         description: Server error
    */
   route.patch('/:id', UserController.updateUserById);
 
@@ -188,23 +167,22 @@ export default (app: Router) => {
    * @swagger
    * /api/v1/users/{id}:
    *   delete:
-   *     summary: Delete user using user_id
+   *     summary: Delete a user by ID
    *     tags: [Users]
    *     parameters:
    *       - in: path
    *         name: id
+   *         required: true
    *         schema:
    *           type: number
-   *         required: true
-   *         description: The user id
-   *
+   *         description: The user ID
    *     responses:
    *       200:
-   *         description: The user was deleted
+   *         description: User deleted successfully
    *       404:
-   *         description: The user was not found
+   *         description: User not found
    *       500:
-   *         description: An error occurred
+   *         description: Server error
    */
   route.delete('/:id', UserController.deleteUserById);
 };
